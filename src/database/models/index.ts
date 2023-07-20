5816c3665a5eeb8179eba81a01a070f5fad8811e
peterihimire <peterihimire@gmail.com>
2023-07-20 23:57:43 +0100

    Worked on some files

--- src/database/models/index.ts
+++ src/database/models/index.ts
@@ -1,21 +1,17 @@
 "use strict";
+import fs from "fs";
+import path from "path";
+import { DataTypes, Sequelize } from "sequelize";
 
-import * as fs from "fs";
-import * as path from "path";
-import { Sequelize, DataTypes, Model } from "sequelize";
-// import { ProcessEnv } from "process";
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.ts")[env];
 
-const basename: string = path.basename(__filename);
-const env: string = (process.env.NODE_ENV as string) || "development";
-const config: any = require(__dirname + "/../config/config.ts")[env];
 const db: any = {};
 
-let sequelize: Sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(
-    process.env[config.use_env_variable] as string,
-    config
-  );
+let sequelize: any;
+if (config.url) {
+  sequelize = new Sequelize(config.url, config);
 } else {
   sequelize = new Sequelize(
     config.database,
@@ -26,23 +22,19 @@ if (config.use_env_variable) {
 }
 
 fs.readdirSync(__dirname)
-  .filter((file: string) => {
+  .filter((file) => {
     return (
       file.indexOf(".") !== 0 &&
       file !== basename &&
-      file.slice(-3) === ".ts" &&
-      file.indexOf(".test.ts") === -1
+      (file.slice(-3) === ".ts" || file.slice(-3) === ".js")
     );
   })
-  .forEach((file: string) => {
-    const model: typeof Model = require(path.join(__dirname, file))(
-      sequelize,
-      DataTypes
-    );
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(sequelize, DataTypes);
     db[model.name] = model;
   });
 
-Object.keys(db).forEach((modelName: string) => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
@@ -53,12 +45,60 @@ db.Sequelize = Sequelize;
 
 export default db;
 
+// "use strict";
 
+// import * as fs from "fs";
+// import * as path from "path";
+// import { Sequelize, DataTypes, Model } from "sequelize";
+// // import { ProcessEnv } from "process";
 
+// const basename: string = path.basename(__filename);
+// const env: string = (process.env.NODE_ENV as string) || "development";
+// const config: any = require(__dirname + "/../config/config.ts")[env];
+// const db: any = {};
 
+// let sequelize: Sequelize;
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(
+//     process.env[config.use_env_variable] as string,
+//     config
+//   );
+// } else {
+//   sequelize = new Sequelize(
+//     config.database,
+//     config.username,
+//     config.password,
+//     config
+//   );
+// }
 
+// fs.readdirSync(__dirname)
+//   .filter((file: string) => {
+//     return (
+//       file.indexOf(".") !== 0 &&
+//       file !== basename &&
+//       file.slice(-3) === ".ts" &&
+//       file.indexOf(".test.ts") === -1
+//     );
+//   })
+//   .forEach((file: string) => {
+//     const model: typeof Model = require(path.join(__dirname, file))(
+//       sequelize,
+//       DataTypes
+//     );
+//     db[model.name] = model;
+//   });
 
+// Object.keys(db).forEach((modelName: string) => {
+//   if (db[modelName].associate) {
+//     db[modelName].associate(db);
+//   }
+// });
 
+// db.sequelize = sequelize;
+// db.Sequelize = Sequelize;
+
+// export default db;
 
 // "use strict";
 
---
+++ src/database/models/user.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    holiday_name: DataTypes.STRING,
+    start_date: DataTypes.STRING,
+    acct_id: DataTypes.STRING,
+    end_date: DataTypes.STRING,
+    exempted_personnels: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
