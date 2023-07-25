042da247c32cd9ec3ca1abef54ecba7e391374bc
peterihimire <peterihimire@gmail.com>
2023-07-26 00:18:11 +0100

    Updated sequelize with seeders

--- dist/src/database/seeders/20230724230037-add-users.js
+++ dist/src/database/seeders/20230724230037-add-users.js
@@ -1,25 +1,42 @@
-'use strict';
-
-/** @type {import('sequelize-cli').Migration} */
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const users = [
+    {
+        id: 1,
+        email: "peterihimire@gmail.com",
+        first_name: "Peter",
+        last_name: "Ihimire",
+        phone: "08060958134",
+        acct_id: "8474893029",
+        password: "testing123",
+        createdAt: "2023-06-25 19:38:02.415+01",
+        updatedAt: "2023-06-25 19:38:02.415+01",
+    },
+    {
+        id: 2,
+        email: "mariaihimire@gmail.com",
+        first_name: "Maria",
+        last_name: "Ihimire",
+        phone: "08060958134",
+        acct_id: "8474811119",
+        password: "testing123",
+        createdAt: "2023-06-25 19:38:02.415+01",
+        updatedAt: "2023-06-25 19:38:02.415+01",
+    },
+];
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
-
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+    up: (queryInterface) => queryInterface.sequelize.transaction(async (transaction) => {
+        // here go all migration changes
+        return Promise.all([
+            // queryInterface.bulkInsert("feed_types", feedTypes, { transaction }),
+            await queryInterface.bulkInsert("users", users, { transaction }),
+        ]);
+    }),
+    down: (queryInterface) => queryInterface.sequelize.transaction(async (transaction) => {
+        // here go all migration undo changes
+        return Promise.all([
+            // queryInterface.bulkDelete("feed_types", null as any, { transaction }),
+            await queryInterface.bulkDelete("users", null, { transaction }),
+        ]);
+    }),
 };
--- src/database/seeders/20230724230037-add-users.ts
+++ src/database/seeders/20230724230037-add-users.ts
@@ -1,15 +1,27 @@
 import { QueryInterface } from "sequelize";
 
-const feedTypes = [
-  { id: "b871a455-fddb-414c-ac02-2cdee07fa671", name: "crypto" },
-  { id: "68b15f90-19ca-4971-a2c6-67e66dc88f77", name: "general" },
-];
-const feeds = [
+const users = [
   {
     id: 1,
-    name: "cointelegraph",
-    url: "https://cointelegraph.com/rss",
-    feed_type_id: "b871a455-fddb-414c-ac02-2cdee07fa671",
+    email: "peterihimire@gmail.com",
+    first_name: "Peter",
+    last_name: "Ihimire",
+    phone: "08060958134",
+    acct_id: "8474893029",
+    password: "testing123",
+    createdAt: "2023-06-25 19:38:02.415+01",
+    updatedAt: "2023-06-25 19:38:02.415+01",
+  },
+  {
+    id: 2,
+    email: "mariaihimire@gmail.com",
+    first_name: "Maria",
+    last_name: "Ihimire",
+    phone: "08060958134",
+    acct_id: "8474811119",
+    password: "testing123",
+    createdAt: "2023-06-25 19:38:02.415+01",
+    updatedAt: "2023-06-25 19:38:02.415+01",
   },
 ];
 
@@ -18,8 +30,8 @@ module.exports = {
     queryInterface.sequelize.transaction(async (transaction) => {
       // here go all migration changes
       return Promise.all([
-        queryInterface.bulkInsert("feed_types", feedTypes, { transaction }),
-        queryInterface.bulkInsert("feeds", feeds, { transaction }),
+        // queryInterface.bulkInsert("feed_types", feedTypes, { transaction }),
+        await queryInterface.bulkInsert("users", users, { transaction }),
       ]);
     }),
 
@@ -27,8 +39,8 @@ module.exports = {
     queryInterface.sequelize.transaction(async (transaction) => {
       // here go all migration undo changes
       return Promise.all([
-        queryInterface.bulkDelete("feed_types", null as any, { transaction }),
-        queryInterface.bulkDelete("feeds", null as any, { transaction }),
+        // queryInterface.bulkDelete("feed_types", null as any, { transaction }),
+        await queryInterface.bulkDelete("users", null as any, { transaction }),
       ]);
     }),
 };
