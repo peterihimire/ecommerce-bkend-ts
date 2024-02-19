b69bf9d4a06ec30bbb457feb407a337f0fab88e5
peterihimire <peterihimire@gmail.com>
2024-02-19 23:21:00 +0100

    Setting up the auth repository

--- dist/src/repositories/user-repository.js
+++ dist/src/repositories/user-repository.js
@@ -1,1 +1,47 @@
 "use strict";
+// import { RequestHandler } from "express";
+// import { httpStatusCodes } from "../utils/http-status-codes";
+// import BaseError from "../utils/base-error";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// import bcrypt from "bcryptjs";
+// import { default as bcrypt } from "bcryptjs";
+// import randomString from "../utils/acc-generator";
+// import { CHARLIST } from "../utils/list-data";
+// import { sign, verify } from "jsonwebtoken";
+// require("dotenv").config();
+const models_1 = __importDefault(require("../database/models"));
+const User = models_1.default.User;
+const Profile = models_1.default.Profile;
+const foundUser = async (email) => {
+    return User.findOne({
+        where: { email: email },
+    });
+};
+const existingAcctId = async (acct_id) => {
+    return User.findOne({
+        where: { acct_id: acct_id },
+    });
+};
+const createUser = async (data) => {
+    return User.create({
+        email: data.email,
+        password: data.password,
+        acct_id: data.acct_id,
+    });
+};
+const createProfile = async (data) => {
+    return Profile.create({
+        first_name: data.first_name,
+        last_name: data.last_name,
+        acct_id: data.acct_id,
+        userId: data.userId,
+    });
+};
+module.exports = {
+    foundUser,
+    existingAcctId,
+    createUser,
+};
---
+++ src/repositories/user-repository.ts
@@ -0,0 +1,57 @@
+// import { RequestHandler } from "express";
+// import { httpStatusCodes } from "../utils/http-status-codes";
+// import BaseError from "../utils/base-error";
+
+// import bcrypt from "bcryptjs";
+// import { default as bcrypt } from "bcryptjs";
+// import randomString from "../utils/acc-generator";
+// import { CHARLIST } from "../utils/list-data";
+// import { sign, verify } from "jsonwebtoken";
+// require("dotenv").config();
+import db from "../database/models";
+const User = db.User;
+const Profile = db.Profile;
+
+const foundUser = async (email: string) => {
+  return User.findOne({
+    where: { email: email },
+  });
+};
+
+const existingAcctId = async (acct_id: string) => {
+  return User.findOne({
+    where: { acct_id: acct_id },
+  });
+};
+
+const createUser = async (data: {
+  email: string;
+  password: string;
+  acct_id: string;
+}) => {
+  return User.create({
+    email: data.email,
+    password: data.password,
+    acct_id: data.acct_id,
+  });
+};
+
+const createProfile = async (data: {
+  first_name: string;
+  last_name: string;
+  acct_id: string;
+  userId: number;
+}) => {
+  return Profile.create({
+    first_name: data.first_name,
+    last_name: data.last_name,
+    acct_id: data.acct_id,
+    userId: data.userId,
+  });
+};
+
+module.exports = {
+  foundUser,
+  existingAcctId,
+  createUser,
+};
