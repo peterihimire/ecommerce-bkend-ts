d1d4b028bf2abb6e6624ef39086412f2f534e88b
peterihimire <peterihimire@gmail.com>
2024-03-22 09:59:45 +0100

    Worked on dockerfile

--- dist/src/routes/admin-auth-route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const admin_auth_controller_1 = require("../controllers/admin-auth-controller");
-const router = (0, express_1.Router)();
-router.post("/register", admin_auth_controller_1.register);
-router.post("/login", admin_auth_controller_1.login);
-exports.default = router;
--- dist/src/utils/acc-generator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const randomString = (length, chars) => {
-    var result = "";
-    for (var i = length; i > 0; --i)
-        result += chars[Math.round(Math.random() * (chars.length - 1))];
-    return result;
-};
-exports.default = randomString;
