f8febdb6ba20a171d0855b04af68d75163d53160
peterihimire <peterihimire@gmail.com>
2024-05-31 21:23:07 +0100

    Made sure dist and data volume binding files not sent to github

--- dist/src/routes/onboard-route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const onboard_controller_1 = require("../controllers/onboard-controller");
-const router = (0, express_1.Router)();
-router.post("/register", onboard_controller_1.register);
-router.post("/verify_email", onboard_controller_1.verify_otp);
-exports.default = router;
