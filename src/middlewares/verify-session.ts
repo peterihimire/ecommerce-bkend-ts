9b8476627c3d8546f9a3c8abe12229dee07952d1
peterihimire <peterihimire@gmail.com>
2024-06-02 22:12:24 +0100

    Romoved images and user-session middleware

--- src/middlewares/verify-session.ts
+++ src/middlewares/verify-session.ts
@@ -76,54 +76,6 @@ export const isLoggedIn: RequestHandler = (req, res, next) => {
   }
   return next();
 };
-// // USER ONLY
-// export const verifySessionAndAuthorization: RequestHandler = (
-//   req,
-//   res,
-//   next
-// ) => {
-//   verifySession(req, res, async () => {
-//     const user = req.user;
-//     const user_id = req.body?.id || req.params?.id;
-
-//     if (!user?.email) {
-//       return next(
-//         new BaseError(
-//           "Not authorised to access resource, invalid or expired session!",
-//           httpStatusCodes.UNAUTHORIZED
-//         )
-//       );
-//     }
-
-//     if (user?.id === user_id || user?.id) {
-//       next();
-//       return;
-//     }
-
-//     return next(
-//       new BaseError(
-//         "Requires User Authorization!",
-//         httpStatusCodes.UNAUTHORIZED
-//       )
-//     );
-//   });
-// };
-
-// // USER & CART ONLY
-// export const verifySessionAndCart: RequestHandler = (req, res, next) => {
-//   const user = req.user;
-//   const user_id = req.body?.id || req.params?.id;
-
-//   if (!user) {
-//     next();
-//     return;
-//   }
-
-//   if (user?.id === user_id || user?.id) {
-//     next();
-//     return;
-//   }
-// };
 
 // ADMIN ONLY
 export const verifySessionAdmin: RequestHandler = (req, res, next) => {
