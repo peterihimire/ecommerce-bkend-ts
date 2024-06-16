27fe43a6595e65dc36288f37d6da4f2effef9183
peterihimire <peterihimire@gmail.com>
2024-06-16 13:57:39 +0100

    Modified the email mobile template responsiveness

--- src/controllers/onboard-controller.ts
+++ src/controllers/onboard-controller.ts
@@ -131,9 +131,9 @@ export const register: RequestHandler = async (req, res, next) => {
                      
                       }
                       .header, .body, .footer {
-                          padding: 10px !important;
-                          padding-bottom: 10px !important;
-                          padding-top: 10px !important;
+                          padding: 0px !important;
+                          padding-bottom: 0px !important;
+                          padding-top: 0px !important;
                       }
                     }
                   </style>
